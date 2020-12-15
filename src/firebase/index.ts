196afda2aebbc6089e9914dc120db3623bb07cb1
André Kuhlmann <andre.kuhlmann@study.hs-duesseldorf.de>
2020-12-15 12:36:09 +0000

    Setup Firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,34 @@
+// This file stablishes the connection to the firebase database
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+// Firebase credentials
+const firebaseConfig = {
+  apiKey: 'AIzaSyBL4mnfw7mMNA85k9ylXrzXjRjaLZcWp9Y',
+  authDomain: 'wetter-station-hsd.firebaseapp.com',
+  databaseURL: 'https://wetter-station-hsd.firebaseio.com',
+  projectId: 'wetter-station-hsd',
+  storageBucket: 'wetter-station-hsd.appspot.com',
+  messagingSenderId: '1079300047838',
+  appId: '1:1079300047838:web:f8582a5371bd2c642ed34f',
+  measurementId: 'G-PQNRM1E3XV'
+}
+
+// Setup firebase
+firebase.initializeApp(firebaseConfig)
+
+// Setup firestore
+const db = firebase.firestore()
+
+// Setup firebase storage
+const storage = firebase.storage()
+
+// === Firebase Collections ===
+
+const stations = db.collection('stations')
+const capabilities = db.collection('capabilities')
+
+// Export reusable modules
+module.exports = { firebase, db, storage, stations, capabilities }
