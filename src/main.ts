1980961e37fc0f66e38a77b8985b3849a9218dc7
André Kuhlmann <andre.kuhlmann@study.hs-duesseldorf.de>
2020-12-18 12:10:34 +0000

    Remove Hello Typescript log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
 // 'use-strict'
 
-console.log('Hello from TypeScript!')
-
 // Start Webserver
 import './server'
 import './firebase/authStateChange.js'
