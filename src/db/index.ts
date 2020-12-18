630c3b97286fa003b355677e202e89fc0f44f134
André Kuhlmann <andre.kuhlmann@study.hs-duesseldorf.de>
2020-12-18 12:12:38 +0000

    Add comment

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import low from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync'
 
+// The db.json file gets created at the projects root directory
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
